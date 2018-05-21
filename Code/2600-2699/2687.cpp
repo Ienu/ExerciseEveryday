7ce88298ab34992aea39a20c1b80fbfc27e547ab
Wenyu <0910692@mail.nankai.edu.cn>
2018-05-21 10:33:56 -0400

    Create 2687.cpp

---
+++ Code/2600-2699/2687.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int n, i;
+	cin >> n;
+	int* data = new int[n];
+	for(i = 0; i < n; ++i) {
+    cin >> data[i];
+  }
+	for(i = n - 1; i >= 0; --i) {
+    cout << data[i] << " ";
+	}
+  cout << endl;
+	delete[] data;
+	return 0;
+}
