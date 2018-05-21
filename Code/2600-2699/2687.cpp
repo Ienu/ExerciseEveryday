8a28b5a14b41e9c37393e23db95eb6997fb4c9a1
Wenyu <0910692@mail.nankai.edu.cn>
2018-05-21 10:35:05 -0400

    Update 2687.cpp

--- Code/2600-2699/2687.cpp
+++ Code/2600-2699/2687.cpp
@@ -5,12 +5,12 @@ int main() {
 	cin >> n;
 	int* data = new int[n];
 	for(i = 0; i < n; ++i) {
-    cin >> data[i];
-  }
-	for(i = n - 1; i >= 0; --i) {
-    cout << data[i] << " ";
+		cin >> data[i];
 	}
-  cout << endl;
+	for(i = n - 1; i >= 0; --i) {
+		cout << data[i] << " ";
+	}
+	cout << endl;
 	delete[] data;
 	return 0;
 }
